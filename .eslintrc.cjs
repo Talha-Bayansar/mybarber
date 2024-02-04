26c1239002da105124b98e18ebc74df6e5b4342f
Talha Bayansar <talha.bayansar@hotmail.com>
2024-02-04 19:43:13 +0100

    ignore sw.js

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,6 @@
 /** @type {import("eslint").Linter.Config} */
 const config = {
+  ignorePatterns: ["**/public/*.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
