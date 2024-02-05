6a2832358180570eea2db6909844c7a6dc848f97
Talha Bayansar <talha.bayansar@hotmail.com>
2024-02-05 11:57:10 +0100

    adding responsiveness to google maps iframe

--- src/app/[locale]/barbershops/[id]/_components/contact.tsx
+++ src/app/[locale]/barbershops/[id]/_components/contact.tsx
@@ -1,4 +1,5 @@
-import { Mail, Phone } from "lucide-react";
+import { LucideIcon, Mail, Phone } from "lucide-react";
+import { getTranslations } from "next-intl/server";
 import { List } from "~/components";
 import { Link } from "~/navigation";
 import { BarbershopRecord } from "~/server/db";
@@ -7,28 +8,44 @@ type Props = {
   barbershop: BarbershopRecord;
 };
 
-export const Contact = ({ barbershop }: Props) => {
+type ContactLine = {
+  href: string;
+  Icon: LucideIcon;
+  value: string;
+};
+
+export const Contact = async ({ barbershop }: Props) => {
+  const t = await getTranslations("global");
   const { address } = barbershop;
+  const contactLines: ContactLine[] = [
+    {
+      href: `tel:${barbershop.phone_number}`,
+      Icon: Phone,
+      value: barbershop.phone_number ?? t("not_specified"),
+    },
+    {
+      href: `mailto:${barbershop.email}`,
+      Icon: Mail,
+      value: barbershop.email ?? t("not_specified"),
+    },
+  ];
+
   return (
     <List className="mt-4">
-      <div className="flex gap-4">
-        <Phone />
-        <Link className="underline" href={`tel:${barbershop.phone_number}`}>
-          {barbershop.phone_number}
-        </Link>
-      </div>
-      <div className="flex gap-4">
-        <Mail />
-        <Link className="underline" href={`mailto:${barbershop.email}`}>
-          {barbershop.email}
-        </Link>
-      </div>
+      {contactLines.map((contactLine) => (
+        <div key={contactLine.value} className="flex gap-4">
+          <contactLine.Icon />
+          <Link className="underline" href={contactLine.href}>
+            {contactLine.value}
+          </Link>
+        </div>
+      ))}
       {address && (
         <iframe
           src={`https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2507.54745786756!2d4.187578376566183!3d51.06144334318395!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c393aa9f57e4eb%3A0xcab006804b64ec87!2s${address.street}%20${address.house_number}%2C%20${address.zip}%20${address.city}!5e0!3m2!1snl!2sbe!4v1707093057876!5m2!1snl!2sbe`}
           width="600"
           height="450"
-          className="w-full"
+          className="h-60 w-full md:h-80"
           style={{ border: 0 }}
           allowFullScreen={true}
           loading="lazy"
