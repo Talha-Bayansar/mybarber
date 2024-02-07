e42473f96d30293ba51a9361e2fa85d781809fa2
Talha Bayansar <talha.bayansar@hotmail.com>
2024-02-07 19:52:45 +0100

    loading barbers and showing in radio group

--- src/app/[locale]/barbershops/[id]/new-reservation/_components/barber-selection.tsx
+++ src/app/[locale]/barbershops/[id]/new-reservation/_components/barber-selection.tsx
@@ -9,33 +9,79 @@ import {
 } from "~/components/ui/form";
 import { type NewReservationForm } from "./reservation-form";
 import { useTranslations } from "next-intl";
-import { Input } from "~/components/ui/input";
+import { api } from "~/trpc/react";
+import { useParams } from "next/navigation";
+import { RadioGroup, RadioGroupItem } from "~/components/ui/radio-group";
+import { List } from "~/components/layout/list";
+import { Skeleton } from "~/components/ui/skeleton";
+import { generateArray } from "~/lib/utils";
 
 type Props = {
   form: NewReservationForm;
 };
 
 export const BarberSelection = ({ form }: Props) => {
-  const t = useTranslations();
+  const t = useTranslations("NewReservationPage");
+  const { id: barbershopId } = useParams<{ id: string }>();
+  const { data: barbers, isLoading: isLoadingBarbers } =
+    api.barber.getByBarbershopId.useQuery({
+      barbershopId,
+    });
 
-  // Barber logic
+  if (isLoadingBarbers) return <BarberSelectionSkeleton />;
 
   return (
     <FormField
       control={form.control}
       name="barberId"
       render={({ field }) => (
-        <FormItem>
-          <FormLabel>{t("global.time")}</FormLabel>
+        <FormItem className="space-y-3">
+          <FormLabel>{t("select_barber")}</FormLabel>
           <FormControl>
-            <Input
-              placeholder={t("NewReservationPage.select_barber")}
-              {...field}
-            />
+            <RadioGroup
+              onValueChange={field.onChange}
+              defaultValue={field.value}
+              className="flex flex-col space-y-1"
+            >
+              <FormItem className="flex items-center space-x-3 space-y-0">
+                <FormControl>
+                  <RadioGroupItem value="" />
+                </FormControl>
+                <FormLabel className="font-normal">
+                  {t("no_preference")}
+                </FormLabel>
+              </FormItem>
+              {barbers?.map((barber) => (
+                <FormItem
+                  key={barber.id}
+                  className="flex items-center space-x-3 space-y-0"
+                >
+                  <FormControl>
+                    <RadioGroupItem value={barber.id} />
+                  </FormControl>
+                  <FormLabel className="font-normal">
+                    {barber.first_name} {barber.last_name}
+                  </FormLabel>
+                </FormItem>
+              ))}
+            </RadioGroup>
           </FormControl>
           <FormMessage />
         </FormItem>
       )}
     />
+  );
+};
+
+const BarberSelectionSkeleton = () => {
+  return (
+    <List>
+      {generateArray(3).map((v) => (
+        <div key={v} className="flex gap-3">
+          <Skeleton className="h-4 w-4 rounded-full" />
+          <Skeleton className="h-4 w-40" />
+        </div>
+      ))}
+    </List>
   );
 };
