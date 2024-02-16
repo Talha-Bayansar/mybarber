c2e6252c7ca60a4c146fa444f71547874cd940a8
Talha Bayansar <talha.bayansar@hotmail.com>
2024-02-16 23:36:20 +0100

    adding new manifest ffor owner

--- src/app/[locale]/(owner-portal)/owner/(root)/layout.tsx
+++ src/app/[locale]/(owner-portal)/owner/(root)/layout.tsx
@@ -1,7 +1,21 @@
+import { type Metadata } from "next";
 import { getTranslations } from "next-intl/server";
 import { Main } from "~/components/layout/main";
 import { PageWrapper } from "~/components/layout/page-wrapper";
 import { Title } from "~/components/layout/title";
+
+export const metadata: Metadata = {
+  generator: "Next.js",
+  manifest: "/owner-manifest.json",
+  keywords: ["My", "Barber", "MyBarber", "Hair", "Haircut", "Reservation"],
+  authors: [
+    { name: "Talha Bayansar" },
+    {
+      name: "Talha Bayansar",
+      url: "https://www.linkedin.com/in/talha-bayansar-17039a19a/",
+    },
+  ],
+};
 
 const OwnerLayout = async ({ children }: { children: React.ReactNode }) => {
   const t = await getTranslations("OwnerPage");
